b92d99920b104c22b17fccb050d00d10c60f29fd
John Goff <john.g@hiredhippo.ai>
2021-02-20 15:06:43 -0500

    create basic read function

--- lib/todo_tex.ex
+++ lib/todo_tex.ex
@@ -1,18 +1,27 @@
 defmodule TodoTex do
   @moduledoc """
-  Documentation for `TodoTex`.
+  Elixir library for reading and writing ToDo files in `todo.txt` format.
+
+  The most basic operation is `TodoTex.read!/1`, which takes a path to a `txt`
+  file in the `todo.txt` format. See [the todo.txt github](https://github.com/todotxt/todo.txt) for a description
+  of the format.
   """
 
+  defstruct [:path, :items]
+
   @doc """
-  Hello world.
+  Reads a `todo.txt` file and returns a `TodoTex` struct with a list of todo items.
 
   ## Examples
 
-      iex> TodoTex.hello()
-      :world
+      TodoTex.read!("/path/to/todo.txt")
+      %TodoTex{path: "/path/to/todo.txt", list: [%TodoTex.Item{}, ...]}
 
   """
-  def hello do
-    :world
+  def read!(path) when is_binary(path) do
+    file = File.open!(path, [:read, :utf8])
+    list = parse_lines(file)
+    File.close(file)
+    %TodoTex{path: path, items: list}
   end
 end
